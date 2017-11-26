421d45a37a7bfd4adcf43ab9f5c2955e53c8f94a
Romain Manni-Bucau <rmannibucau@apache.org>
2017-11-26 17:45:15 +0100

    formatting code base

--- src/main/java/org/talend/sdk/component/api/configuration/action/Validable.java
+++ src/main/java/org/talend/sdk/component/api/configuration/action/Validable.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+ * Copyright (C) 2006-2017 Talend Inc. - www.talend.com
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.talend.sdk.component.api.configuration.action;
 
@@ -47,22 +47,41 @@ public @interface Validable {
      *
      * And this model:
      *
-     * {@code public class MyModel { @Validable(value = "xxx", parameters = {".", "foo"}) @Option String something; @Option String foo; }}
+     * {@code public class MyModel { @Validable(value = "xxx", parameters = {".", "foo"}) @Option String something; @Option String
+     * foo; }}
      *
      * This must call doValidate(something, foo).
      *
      * Syntax is the following:
      *
      * <ul>
-     *     <li>.: represents the decorated option (aka "this")</li>
-     *     <li>../foo: represents the <pre>foo</pre> option of the parent (if exists) of "."</li>
-     *     <li>bar: represents the <pre>bar</pre> sibling option of the decorated field</li>
-     *     <li>bar/dummy: represents the <pre>dummy</pre> option of the child bar of the decorated field</li>
+     * <li>.: represents the decorated option (aka "this")</li>
+     * <li>../foo: represents the
+     * 
+     * <pre>
+     * foo
+     * </pre>
+     * 
+     * option of the parent (if exists) of "."</li>
+     * <li>bar: represents the
+     * 
+     * <pre>
+     * bar
+     * </pre>
+     * 
+     * sibling option of the decorated field</li>
+     * <li>bar/dummy: represents the
+     * 
+     * <pre>
+     * dummy
+     * </pre>
+     * 
+     * option of the child bar of the decorated field</li>
      * </ul>
      *
      * This syntax is close to path syntax but the important point is all the parameters are related to the decorated option.
      *
      * @return parameters for the validation.
      */
-    String[] parameters() default {"."};
+    String[] parameters() default { "." };
 }
