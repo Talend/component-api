6e9c096a1cfb0ad0d3a356a7e4be4c3f60064ad5
Anas KHABALI <akhabali@talend.com>
2018-04-18 10:55:08 +0100

    extend the http client api to handle more usecases

--- src/main/java/org/talend/sdk/component/api/service/http/Url.java
+++ src/main/java/org/talend/sdk/component/api/service/http/Url.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,15 +15,17 @@
  */
 package org.talend.sdk.component.api.service.http;
 
-/**
- * You can cast an injected client with this type to initialize the base.
- */
-public interface HttpClient {
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-    /**
-     * set the base URI of the http request
-     *
-     * @param base the base URI of the http client
-     */
-    void base(String base);
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Mark parameter as the URL of the http request
+ */
+@Target(PARAMETER)
+@Retention(RUNTIME)
+public @interface Url {
+
 }
