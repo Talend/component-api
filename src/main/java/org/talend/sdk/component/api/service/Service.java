c8455542b0cfb23d10825735024dc62222000a32
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-08 10:22:49 +0200

    TCOMP-855 ensure @Service can be put on a field

--- src/main/java/org/talend/sdk/component/api/service/Service.java
+++ src/main/java/org/talend/sdk/component/api/service/Service.java
@@ -15,13 +15,14 @@
  */
 package org.talend.sdk.component.api.service;
 
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target(TYPE)
+@Target({ TYPE, FIELD })
 @Retention(RUNTIME)
 public @interface Service {
 }
