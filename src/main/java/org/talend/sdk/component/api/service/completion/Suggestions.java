ebb307cb071fd97c898265c2ea42b41ac5b329cc
Ivan Honchar <ivan.honchar@globallogic.com>
2018-06-11 12:34:46 +0300

    fix: remove unused imports from Suggestable and Suggestions

--- src/main/java/org/talend/sdk/component/api/service/completion/Suggestions.java
+++ src/main/java/org/talend/sdk/component/api/service/completion/Suggestions.java
@@ -21,7 +21,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import org.talend.sdk.component.api.configuration.action.Proposable;
 import org.talend.sdk.component.api.meta.Documentation;
 import org.talend.sdk.component.api.service.ActionType;
 
