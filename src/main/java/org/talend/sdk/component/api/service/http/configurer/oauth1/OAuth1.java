e709e42d6a9e79d0fcd0eb5b5db7acfc0c729c2a
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-09-06 08:46:52 +0200

    TCOMP-1050 moving oauth1 logic in runtime

--- src/main/java/org/talend/sdk/component/api/service/http/configurer/oauth1/OAuth1.java
+++ src/main/java/org/talend/sdk/component/api/service/http/configurer/oauth1/OAuth1.java
@@ -15,30 +15,12 @@
  */
 package org.talend.sdk.component.api.service.http.configurer.oauth1;
 
-import static java.util.Locale.ROOT;
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.joining;
 import static lombok.AccessLevel.PRIVATE;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.Signature;
-import java.security.SignatureException;
-import java.util.Base64;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Objects;
-import java.util.TreeMap;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import java.util.ServiceLoader;
 
 import lombok.Builder;
 import lombok.Data;
@@ -51,99 +33,31 @@ public final class OAuth1 {
 
         @Override
         public void configure(final Connection connection, final Configurer.ConfigurerConfiguration configuration) {
-            final Configuration oauth1Config = Stream.of(configuration.configuration()).filter(Configuration.class::isInstance)
-                    .findFirst().map(Configuration.class::cast)
-                    .orElseThrow(() -> new IllegalArgumentException("No OAuth1.Configuration @ConfigurerOption set"));
-
-            final Map<String, String> values = new OAuth1().buildParameters(connection.getMethod(), connection.getUrl(),
-                    connection.getPayload(), oauth1Config);
-
-            final String authorization = ofNullable(oauth1Config.getHeaderPrefix()).orElse("OAuth ")
-                    + values.entrySet().stream().filter(e -> e.getKey().startsWith("oauth_"))
-                            .map(e -> e.getKey() + "=\"" + e.getValue() + "\"").collect(joining(", "));
-            connection.withHeader(ofNullable(oauth1Config.getHeader()).orElse("Authorization"), authorization);
+            loadProvider().newConfigurer().configure(connection, configuration);
         }
     }
 
     public Map<String, String> buildParameters(final String method, final String url, final byte[] payload,
             final OAuth1.Configuration oauth1Config) {
-        final String algorithm = ofNullable(oauth1Config.getAlgorithm()).orElse("HMAC-SHA1");
-        final Map<String, String> values = new TreeMap<>();
-        values.put("oauth_consumer_key", oauth1Config.getConsumerKey());
-        values.put("oauth_nonce", ofNullable(oauth1Config.getNonce()).orElseGet(this::newNonce));
-        values.put("oauth_signature_method", algorithm);
-        values.put("oauth_timestamp", ofNullable(oauth1Config.getTimestamp()).map(String::valueOf)
-                .orElseGet(() -> Long.toString(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()))));
-        values.put("oauth_version", "1.0");
-        ofNullable(oauth1Config.getToken()).ifPresent(token -> values.put("oauth_token", token));
-        ofNullable(oauth1Config.getPayloadHashAlgorithm())
-                .ifPresent(algo -> values.put("oauth_body_hash", hash(algo, ofNullable(payload).orElseGet(() -> new byte[0]))));
-        values.entrySet().forEach(e -> e.setValue(encode(e.getValue())));
-
-        final String signature = sign(algorithm, signingString(values, method, url), oauth1Config);
-        values.put("oauth_signature", signature);
-        return values;
+        return loadProvider().buildParameters(method, url, payload, oauth1Config);
     }
 
-    private String hash(final String algo, final byte[] payload) {
-        if ("plain".equalsIgnoreCase(algo)) {
-            return Base64.getEncoder().encodeToString(payload);
+    private static OAuth1Provider loadProvider() {
+        final Iterator<OAuth1Provider> iterator = ServiceLoader.load(OAuth1Provider.class).iterator();
+        if (!iterator.hasNext()) {
+            throw new IllegalStateException("No registered implementation of OAuth1Provider");
         }
-        try {
-            final MessageDigest digest = MessageDigest.getInstance(algo);
-            return Base64.getEncoder().encodeToString(digest.digest(payload));
-        } catch (final NoSuchAlgorithmException e) {
-            throw new IllegalArgumentException("Invalid hashing computation using algorithm: " + algo, e);
-        }
+        return iterator.next();
     }
 
-    private String sign(final String algorithm, final String signingString, final Configuration configuration) {
-        if (algorithm.toLowerCase(ROOT).contains("hmac")) {
-            final byte[] signingKey = ofNullable(configuration.getSigningHmacKey())
-                    .orElseGet(() -> Stream.of(configuration.getConsumerSecret(), configuration.getTokenSecret())
-                            .filter(Objects::nonNull).map(this::encode).collect(joining("&")).getBytes(StandardCharsets.UTF_8));
-            try {
-                final SecretKeySpec key = new SecretKeySpec(signingKey, algorithm);
-                final Mac mac = Mac.getInstance(key.getAlgorithm().replace("-", ""));
-                mac.init(key);
-                return encode(Base64.getEncoder().encodeToString(mac.doFinal(signingString.getBytes(StandardCharsets.UTF_8))));
-            } catch (final InvalidKeyException | NoSuchAlgorithmException e) {
-                throw new IllegalStateException(e);
-            }
-        } else {
-            try {
-                final Signature signature = Signature.getInstance(algorithm.replace("-", ""));
-                signature.initSign(configuration.getSigningSignatureKey());
-                signature.update(signingString.getBytes(StandardCharsets.UTF_8));
-                return encode(Base64.getEncoder().encodeToString(signature.sign()));
-            } catch (final SignatureException | InvalidKeyException | NoSuchAlgorithmException e) {
-                throw new IllegalArgumentException(e);
-            }
-        }
-    }
+    /**
+     * This is the SPI to load the implementation, WARNING: this is an internal API.
+     */
+    public interface OAuth1Provider {
 
-    private String signingString(final Map<String, String> values, final String method, final String url) {
-        return method.toUpperCase(ROOT) + "&" + encode(stripQuery(url)) + "&" + encode(
-                values.entrySet().stream().map(e -> String.format("%s=%s", e.getKey(), e.getValue())).collect(joining("&")));
-    }
+        Map<String, String> buildParameters(String method, String url, byte[] payload, OAuth1.Configuration oauth1Config);
 
-    private String stripQuery(final String url) {
-        if (url.contains("?")) {
-            return url.substring(0, url.indexOf('?'));
-        }
-        return url;
-    }
-
-    private String encode(final String value) {
-        try {
-            return URLEncoder.encode(value, "UTF-8").replace("+", "%20").replace("*", "%2A").replace("%7E", "~");
-        } catch (final UnsupportedEncodingException uee) {
-            throw new IllegalStateException(uee.getMessage(), uee);
-        }
-    }
-
-    private String newNonce() {
-        return UUID.randomUUID().toString().replace("-", "");
+        org.talend.sdk.component.api.service.http.Configurer newConfigurer();
     }
 
     @Data
