0b6840f022620acfb2299de6402ac3298bee1ad1
Romain Manni-Bucau <rmannibucau@gmail.com>
2019-01-10 18:52:46 +0100

    TCOMP-1256 add mapDescriptorToClassLoader in Resolver

--- src/main/java/org/talend/sdk/component/api/service/dependency/Resolver.java
+++ src/main/java/org/talend/sdk/component/api/service/dependency/Resolver.java
@@ -15,11 +15,70 @@
  */
 package org.talend.sdk.component.api.service.dependency;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.List;
 
 public interface Resolver {
 
+    /**
+     * Creates a classloader from the passed descriptor (dependencies.txt).
+     *
+     * WARNING: note it is very important to close the descriptor once no more used otherwise
+     * you can leak memory.
+     *
+     * @param descriptor the dependencies.txt InputStream.
+     * @return the classloader initialized with the resolved dependencies.
+     */
+    ClassLoaderDescriptor mapDescriptorToClassLoader(InputStream descriptor);
+
+    /**
+     * Alias to load dependencies from a plain list of gav (groupId:artifactId:version).
+     *
+     * @param gavs the dependencies to use to resolve dependencies.
+     * @return the collection of file representing the available dependencies.
+     */
+    default ClassLoaderDescriptor mapDescriptorToClassLoader(final List<String> gavs) {
+        return mapDescriptorToClassLoader(new ByteArrayInputStream(String.join("\n", gavs).getBytes(StandardCharsets.UTF_8)));
+    }
+
+    /**
+     * Resolves the dependencies from the descriptor passed as an InputStream.
+     *
+     * IMPORTANT: this is to use when you are sure the file is resolvable if you don't have a fallback.
+     * In that last case, prefer the <code>mapDescriptorToClassLoader</code>.
+     *
+     * @param descriptor the dependencies.txt to use to resolve dependencies.
+     * @return the collection of file representing the available dependencies.
+     */
     Collection<File> resolveFromDescriptor(InputStream descriptor);
+
+    /**
+     * Alias to load dependencies from a plain list of gav (groupId:artifactId:version).
+     *
+     * @param gavs the dependencies to use to resolve dependencies.
+     * @return the collection of file representing the available dependencies.
+     */
+    default Collection<File> resolveFromDescriptor(final List<String> gavs) {
+        return resolveFromDescriptor(new ByteArrayInputStream(String.join("\n", gavs).getBytes(StandardCharsets.UTF_8)));
+    }
+
+    /**
+     * Abstract a classloader adding the metadata about the resolution done to create it.
+     */
+    interface ClassLoaderDescriptor extends AutoCloseable {
+
+        /**
+         * @return the underlying classloader.
+         */
+        ClassLoader asClassLoader();
+
+        /**
+         * @return the dependencies who matched the resolution and were used to create the classloader.
+         */
+        Collection<String> resolvedDependencies();
+    }
 }
